0b9c545477d8d7b0771b3724ddd30631855ee9d5
f.taleb <farzanehtaleb11@gmail.com>
2017-07-17 15:41:33 +0430

    74

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-/*
-  directives: [ROUTER_DIRECTIVES]
-*/
+
 })
-export class UserComponent  {
+export class UserComponent implements OnDestroy {
 
-  constructor(private  router: Router) { }
+  private subscription: Subscription ;
+id: string ;
+  constructor(private  router: Router , private activatedRow: ActivatedRoute) {
 
-
-
+    // this.id = activatedRow.snapshot.params['id'] ;
+  this.subscription =  activatedRow.params.subscribe(
+      (param: any) => this.id = param['id']
+    ) ;
+  }
   onNavigate() {
-
-    this.router.navigate(['/']);
-
+ this.router.navigate(['/']);
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }

e9e37270bb2548927072eb429207cf2b1b6d7085
f.taleb <farzanehtaleb11@gmail.com>
2017-07-19 12:47:28 +0430

    77

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent implements OnDestroy {
+  private subscription: Subscription ;
+  param: string ;
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe() ;
   }
+
+constructor(router: Router) {
+this.subscription = router.routerState.root.queryParams.subscribe(
+  (queryParam: any) => this.param = queryParam['analytics']
+);
+}
 
 }
